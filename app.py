dfa75b2058fe678d0d2493e85c96eb6ab7f55446
Mike Heavers <mheavers@gmail.com>
2024-02-14 15:52:50 -0800

    check for pw in order to generate image

--- app.py
+++ app.py
@@ -8,16 +8,32 @@ from dotenv import load_dotenv
 load_dotenv()
 
 openai_key = os.getenv("OPENAI_API_KEY")
+pw_key = os.getenv("PW")
 
 if openai_key == "<YOUR_OPENAI_API_KEY>":
     openai_key = ""
+
+if pw_key == "<YOUR_PW>":
+    pw_key = ""
+
+if pw_key == "":
+    sys.exit("Please Provide A Password in the Environment Variables")
 
 if openai_key == "":
     sys.exit("Please Provide Your OpenAI API Key")
 
 
-def generate_image(text, model, quality, size):
+
+def generate_image(text, pw):
+    # add a conditional to check for a valid password
+
+    if pw != os.getenv("PW"):
+        # output an error message to the user in the gradio interface if password is invalid
+        raise gr.Error("Invalid password. Please try again.")
+
     try:
+
+
         client = OpenAI(api_key=openai_key)
 
         response = client.images.generate(
@@ -29,22 +45,23 @@ def generate_image(text, model, quality, size):
         )
     except Exception as error:
         print(str(error))
-        raise gr.Error("An error occurred while generating speech. Please check your API key and come back try again.")
+        raise gr.Error("An error occurred while generating the image.")
 
     return response.data[0].url
 
 
 with gr.Blocks() as demo:
     gr.Markdown("# <center> Prompt de Resistance Image Generator</center>")
-    gr.Markdown("**Instructions**: Generate an image from the text prompt below, then click the download arrow from the top right of the image to save it.")
-
+    gr.Markdown("**Instructions**: To use this service, please enter the password. Then generate an image from the prompt field below, then click the download arrow from the top right of the image to save it.")
+    pw = gr.Textbox(label="Password", type="password",
+      placeholder="Enter the password to unlock the service")
     text = gr.Textbox(label="What do you want to create?",
-                      placeholder="Enter your text and then click on the \"Image Generate\" button, "
-                                  "or simply press the Enter key.")
+      placeholder="Enter your text and then click on the \"Image Generate\" button, "
+        "or simply press the Enter key.")
     btn = gr.Button("Generate Image")
     output_image = gr.Image(label="Image Output")
 
-    text.submit(fn=generate_image, inputs=[text], outputs=output_image, api_name="generate_image")
-    btn.click(fn=generate_image, inputs=[text], outputs=output_image, api_name=False)
+    text.submit(fn=generate_image, inputs=[text,pw], outputs=output_image, api_name="generate_image")
+    btn.click(fn=generate_image, inputs=[text,pw], outputs=output_image, api_name=False)
 
 demo.launch(share=True)
